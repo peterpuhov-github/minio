ea900706b78351f99ec82aeed49add4086d4bc2b
Peter Puhov <peterpuhov.github@gmail.com>
2020-09-21 12:04:46 -0400

    Async stdout capturing

--- cmd/object-handlers.go
+++ cmd/object-handlers.go
@@ -541,19 +541,79 @@ func (api objectAPIHandlers) GetCustomObjectContentHandler(w http.ResponseWriter
 	//buf.WriteString("End\n")
 
 	cmd := exec.Command("/build/ndp/dikeSQL")
-	cmd.Stdin = strings.NewReader(string(dump))
-	var stdout bytes.Buffer
-	cmd.Stdout = &stdout
-	var stderr bytes.Buffer
-	cmd.Stderr = &stderr
+	stdoutCaptureAndSend := func(r io.Reader) error {
+		b := new(bytes.Buffer)
+		b.Reset()
 
-	err1 := cmd.Run()
-	if err1 != nil {
-		buf.WriteString("Terminated with error: ")
-		buf.WriteString(stderr.String())
-	} else {
-		buf.WriteString(stdout.String())
+		for {
+			n, err := r.Read(b.Bytes())
+			if n > 0 {
+				writer.SendRecord(b)
+			}
+			if err != nil {
+				// Read returns io.EOF at the end of file, which is not an error for us
+				if err == io.EOF {
+					err = nil
+				}
+				return err
+			}
+		}
 	}
+
+	//var stdout, stderr []byte
+	//var stdout, stderr string
+	var errStdout, errStderr error
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+	err = cmd.Start()
+	if err != nil {
+		//log.Fatalf("cmd.Start() failed with '%s'\n", err)
+		buf.WriteString("cmd.Start() failed with error\n")
+	}
+
+	// cmd.Wait() should be called only after we finish reading
+	// from stdoutIn and stderrIn.
+	// wg ensures that we finish
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		errStdout = stdoutCaptureAndSend(stdoutIn)
+		wg.Done()
+	}()
+
+	//stderr, errStderr = copyAndCapture(os.Stderr, stderrIn)
+	errStderr = stdoutCaptureAndSend(stderrIn)
+
+	wg.Wait()
+
+	err = cmd.Wait()
+	if err != nil {
+		//log.Fatalf("cmd.Run() failed with %s\n", err)
+		buf.WriteString("cmd.Run() failed with error\n")
+	}
+
+	if errStdout != nil || errStderr != nil {
+		//log.Fatal("failed to capture stdout or stderr\n")
+		buf.WriteString("failed to capture stdout or stderr\n")
+	}
+
+	//outStr, errStr := string(stdout), string(stderr)
+
+	/*
+		cmd.Stdin = strings.NewReader(string(dump))
+		var stdout bytes.Buffer
+		cmd.Stdout = &stdout
+		var stderr bytes.Buffer
+		cmd.Stderr = &stderr
+
+		err1 := cmd.Run()
+		if err1 != nil {
+			buf.WriteString("Terminated with error: ")
+			buf.WriteString(stderr.String())
+		} else {
+			buf.WriteString(stdout.String())
+		}
+	*/
 
 	writer.SendRecord(buf)
 	writer.Finish(-1, -1)
